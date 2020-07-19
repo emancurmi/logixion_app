fa691c9a92e241b408f437455670be404aa97e1a
Eman Curmi <emancurmi@gmail.com>
2020-07-19 04:05:39 -0400

    typos about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -11,8 +11,8 @@ export default class About extends Component {
                 <h2>How to Use</h2>
                 <p>Currently this app is to be used by developers who are creating a forms on a website. We are currently developing a more user friendly for users to be able to use it on their own.</p>
                 <ol>
-                    <li>first step is to make a list of all the ids of input and button fields that you would like to put in the tutorial</li>
-                    <li>register or login on this site</li>
+                    <li>First step is to make a list of all the ids of input and button fields that you would like to put in the tutorial</li>
+                    <li>Register or login on this site</li>
                     <li>Create a new tutorial</li>
                     <li>Start adding the steps one after the other</li>
                     <li>Title - Will show as header of the tooltip</li>
